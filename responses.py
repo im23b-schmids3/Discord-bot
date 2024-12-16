6ecb62b93758a143d54dc73232eea907a7d4963b
schmids3 <schmids3@bzz.ch>
2024-12-16 21:39:32 +0100

    small changes for responses

--- responses.py
+++ responses.py
@@ -2,12 +2,15 @@ import datetime
 
 
 def get_response(user_message):
-    sigma = user_message.lower()
+    user_input = user_message.lower()
 
-    if sigma == "!hello":
+    if user_input == "!help" or user_input == "!commands":
+        return "I can respond to the following commands:\n!help, !commands\n\n!hello\n!time"
+
+    elif user_input == "!hello":
         return 'Hey'
 
-    elif sigma == "!time":
+    elif user_input == "!time":
         now = datetime.datetime.now()
         return f'The current time is {now.strftime("%d-%m-%Y %H:%M")}'
 
