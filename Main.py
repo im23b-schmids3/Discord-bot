1edec1b7f266eca3f6610fb838a7fe1ea1891215
schmids3 <schmids3@bzz.ch>
2024-12-21 23:57:51 +0100

    add class for responses

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
-from discord import Intents, Client
+from discord import Intents
 from typing import Final
 import os
 from dotenv import load_dotenv
-from responses import get_response
+from responses import Response
+from discord.ext import commands
+
 
 load_dotenv()
 
 TOKEN: Final = os.getenv('DISCORD_TOKEN')
 
+response_handler = Response()
 intents = Intents.default()
 intents.message_content = True
-client = Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 
 async def send_message(message, user_message):
     if not user_message:
         return
     try:
-        response = get_response(user_message)
+        response = response_handler.get_response(user_message)
         await message.channel.send(response)
     except Exception as e:
         print(f'Error: {e}')
@@ -36,7 +39,7 @@ async def on_message(message):
 
 
 def main():
-    client.run(token=TOKEN)
+    client.run(TOKEN)
 
 
 if __name__ == '__main__':
--- responses.py
+++ responses.py
@@ -1,37 +1,32 @@
 import datetime
 
 
-def get_response(user_message):
-    user_input = {
-        '!help': help,
-        '!hello': hello,
-        '!time': time,
-        '!date': date,
-        "!test": test,
-    }
-    func = user_input.get(user_message.lower())
-    if func:
-        return func()
-    return "Command not recognized. Type !help for a list of commands."
+class Response:
+    def __init__(self):
+        self.user_input = {
+            '!help': self.help,
+            '!hello': self.hello,
+            '!time': self.time,
+            '!date': self.date,
+        }
 
+    def get_response(self, user_message):
+        func = self.user_input.get(user_message.lower())
+        if func:
+            return func()
+        else:
+            return 'Command not recognized. Type !help for a list of commands.'
 
-def help():
-    return 'I can respond to the following commands:\n!help\n\n!hello\n!time\n!date'
+    def help(self):
+        return 'I can respond to the following commands:\n!help\n\n!hello\n!time\n!date'
 
+    def hello(self):
+        return 'Hey'
 
-def hello():
-    return 'Hey'
+    def time(self):
+        now = datetime.datetime.now()
+        return f'The current time is {now.strftime("%H:%M:%S")}'
 
-
-def time():
-    now = datetime.datetime.now()
-    return f'The current time is {now.strftime("%H:%M:%S")}'
-
-
-def date():
-    now = datetime.datetime.now()
-    return f'The current date is {now.strftime("%d-%m-%Y")}'
-
-
-def test():
-    return 'Testing file'
+    def date(self):
+        now = datetime.datetime.now()
+        return f'The current date is {now.strftime("%d-%m-%Y")}'
